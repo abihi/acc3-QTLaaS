f5f8aaf753ef81086e752f3578501a21ee8601e9
Ahmed Bihi <ahmed.bihi@hotmail.com>
2018-10-24 13:49:49 +0200

    typo again

--- add-userdata/ssc-instance-userdata.py
+++ add-userdata/ssc-instance-userdata.py
@@ -98,7 +98,7 @@ while inst_status_master == 'BUILD' or inst_status_worker == 'BUILD':
 print "Instance: "+ instance_master.name +" is in "+ inst_status_master +" state"
 if floating_ip.ip != None:
     instance_master.add_floating_ip(floating_ip)
-    print "Added floation ip: "+ floating_ip.ip +" to "+ instance_master.name
+    print "Added floating ip: "+ floating_ip.ip +" to "+ instance_master.name
 
 print "Instance: "+ instance_worker.name +" is in "+ inst_status_worker +" state"
 
