7751c0ea6e210239990b8d59c77ed5b14a8a78d9
moamattsson <moa_mattsson@hotmail.com>
2018-10-25 16:11:49 +0200

    changed var for IP to asible

--- add-userdata/ansible-commands.sh
+++ add-userdata/ansible-commands.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ansibleIPhost=$(openstack server list --name acc3-ansible-op -c Networks -f value | awk '{ split($5, v, "=");split(v[2],x,","); print x[1], "acc3-ansible-op"}')
-scp -i /home/ubuntu/optimized-test/add-userdata/keys/id_rsa /home/ubuntu/hosts ubuntu@$ansibleIPhost:/etc/hosts
-scp -i /home/ubuntu/optimized-test/add-userdata/keys/id_rsa /home/ubuntu/ansible-hosts ubuntu@$ansibleIPhost:/etc/ansible/hosts
+ansibleIP=$(echo $ansibleIPhost | awk '{split($1, v, " ");print v[1]}')
+scp -i /home/ubuntu/optimized-test/add-userdata/keys/id_rsa /home/ubuntu/hosts ubuntu@$ansibleIP:/etc/hosts
+scp -i /home/ubuntu/optimized-test/add-userdata/keys/id_rsa /home/ubuntu/ansible-hosts ubuntu@$ansibleIP:/etc/ansible/hosts
