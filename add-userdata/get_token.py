55ef90434b733374a4ca3691a9932b0639565a2f
abihi <ahmed.bihi@hotmail.com>
2018-10-26 11:04:44 +0000

    get_token

--- add-userdata/get_token.py
+++ add-userdata/get_token.py
@@ -1,8 +1,9 @@
-
-file = open(“ansible-playbook-output.txt”, “r”)
-file_lines = file.readlines():
+file = open('ansible-playbook-output.txt', 'r')
+file_lines = file.readlines()
 tokenStr = "token.stdout_lines"
 for x in range(len(file_lines)):
     if tokenStr in file_lines[x]:
-        print file_lines[x+1]
-        break
+        token = file_lines[x+1].split('\\')[3]
+        print token[1:len(token)]
+	break
+
