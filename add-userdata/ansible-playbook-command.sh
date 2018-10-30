95c2700bc1b239ac9015f6ac658093d06068c18a
abihi <ahmed.bihi@hotmail.com>
2018-10-30 20:44:52 +0000

    start playbook later

--- add-userdata/ansible-playbook-command.sh
+++ add-userdata/ansible-playbook-command.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ansible-playbook -s --private-key /home/ubuntu/.ssh/id_rsa --start-at-task="adding paths" /home/ubuntu/QTLaaS/spark_deployment.yml
+ansible-playbook -s --private-key /home/ubuntu/.ssh/id_rsa --start-at-task="R  spark integration" /home/ubuntu/QTLaaS/spark_deployment.yml
